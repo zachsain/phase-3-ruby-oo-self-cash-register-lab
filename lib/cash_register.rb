87286b6cf1fc2a4f984c91dd5a8e769c24f6b0a7
Zachary Sain <zachsain@gmail.com>
2022-03-31 18:57:48 -0500

    commit tree ba15a49f06c84c4f723588da5dd03fa736ea572e

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
+class CashRegister
+    attr_reader = :total, :discount
 
+
+    def initialize(items = [])
+        @total = items 
+        
+    end  
+
+    # def cash_register_with_discount
+    # end 
+end 
